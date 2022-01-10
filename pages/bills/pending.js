8c8ccc710e55a290c8cded0630ba085962b4cb20
Tuan Nguyen <ntuan.2502@gmail.com>
2022-01-10 12:48:45 +0700

    add total prices for bills view

--- pages/bills/pending.js
+++ pages/bills/pending.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { Router, useRouter } from "next/router";
 import { useEffect } from "react";
 import Layout from "../../components/Layout";
-import { dateFormat } from "../../lib/lib";
+import { currencyFormat, dateFormat } from "../../lib/lib";
 
 export const getServerSideProps = async (ctx) => {
   const session = await getSession(ctx);
@@ -37,20 +37,24 @@ const pendingPage = ({ bills }) => {
       <div className="flex justify-between pr-6 pl-6 pt-4 pb-4 ">
         <h1 className="pt-2 text-2xl font-bold">Pending</h1>
       </div>
-      <div className="grid grid-cols-5 pr-6 pl-6 pt-2 pb-2 ">
+      <div className="grid grid-cols-6 pr-6 pl-6 pt-2 pb-2 ">
         <div className="text-gray-400">ID</div>
         <div className="place-self-center text-gray-400">Table</div>
         <div className="place-self-center text-gray-400">Status</div>
+        <div className="place-self-center text-gray-400">Total Prices</div>
         <div className="col-span-2 place-self-center text-gray-400">Time</div>
       </div>
 
       {bills.map((bill, key) => (
-        <div key={key} className="grid grid-cols-5 pr-6 pl-6 pt-6 pb-6">
+        <div key={key} className="grid grid-cols-6 pr-6 pl-6 pt-6 pb-6">
           <Link href={`/bills/${bill.id}`}>
             <div className="cursor-pointer">{bill.id}</div>
           </Link>
           <div className="place-self-center">{bill.table.name}</div>
           <div className="place-self-center">{bill.status}</div>
+          <div className="place-self-center">
+            {currencyFormat(bill.total_prices)}
+          </div>
           <div className="flex-wrap col-span-2 place-self-center">
             {dateFormat(bill.createdAt)}
           </div>
